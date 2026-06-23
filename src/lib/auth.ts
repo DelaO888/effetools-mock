2d3850792c4ac550b17e1f7a351f8de933e89af0
Ivan <ivan.delao@prompt.com.mx>
2026-06-23 13:30:41 -0600

    fixing undefined variables

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,33 +1,38 @@
-import { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { prisma } from "./prisma"
-import bcrypt from "bcryptjs"
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "./prisma";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: {label: "Email", type: "email"},
-                password: {label: "Password", type: "password"},
-            },
-            async authorize(credentials: Record<"email" | "password", string> | undefined){
-                if(!credentials.email || !credentials?.password) return null
-                
-                const user = await prisma.user.findUnique({
-                    where: {email: credentials.email},
-                })
+  providers: [
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(
+        credentials: Record<"email" | "password", string> | undefined,
+      ) {
+        if (!credentials?.email || !credentials?.password) return null;
 
-                if(!user) return null
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
 
-                const passwordMatch = await bcrypt.compare(credentials.password, user.password)
-                if (!passwordMatch) return null
+        if (!user) return null;
 
-        return { id: user.id, email: user.email, name: user.name }
-            }
-        })
-    ],
-    session: { strategy: "jwt" },
+        const passwordMatch = await bcrypt.compare(
+          credentials.password,
+          user.password,
+        );
+        if (!passwordMatch) return null;
+
+        return { id: user.id, email: user.email, name: user.name };
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
   pages: { signIn: "/login" },
   secret: process.env.NEXTAUTH_SECRET,
-}
+};
